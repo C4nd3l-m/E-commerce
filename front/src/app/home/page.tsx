5889c6627d4aaa543b435af01725a1bc971c1efa
C4nd3l-m <c4nd3l.m@gmail.com>
2025-01-25 22:20:29 -0300

    fix deploy responsive

--- front/src/app/home/page.tsx
+++ front/src/app/home/page.tsx
@@ -2,11 +2,11 @@ import Card from "@/components/Card/Card";
 
 const Home: React.FC = () => {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen">
-            <h1 className="text-5xl font-semibold text-center text-white mb-8">
+        <div className="flex flex-col items-center justify-center min-h-screen px-4">
+            <h1 className="text-3xl sm:text-4xl md:text-5xl font-semibold text-center text-white mb-6 sm:mb-8">
                 Hello, Apple intelligence.
             </h1>
-            <div>
+            <div className="w-full max-w-3xl">
                 <Card />
             </div>
         </div>
